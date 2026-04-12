3a0fcf9ffccec425f4bc1a0ff4ed7723ce6c1057
Luiz Bertoni <luiz.rcb@gmail.com>
2026-04-12 00:12:49 -0300

    feat: :sparkles: Add rainbowlands calendar to Calendaria

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,23 @@
-Hooks.once('init', async function() {
+import { rainbowlandsCalendar } from "./rainbowlands.js";
 
+const CALENDAR_ID = "rainbowlands";
+
+Hooks.once("init", async function () {});
+
+Hooks.once("ready", async function () {});
+
+Hooks.on("calendaria.init", async () => {
 });
 
-Hooks.once('ready', async function() {
+Hooks.on("calendaria.ready", async ({ api, calendar, version }) => {
+  if (!game.user.isGM) return;
 
+  if (calendar.metadata.id === CALENDAR_ID) return;
+
+  const addedCalendar = await api.addCalendar(
+    CALENDAR_ID,
+    rainbowlandsCalendar,
+  );
+
+  await api.switchCalendar(CALENDAR_ID);
 });
